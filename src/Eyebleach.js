f6c141d7b8ebeb0440e312f56d8ba39e4e260cf0
Shivam Chhapola <38777996+shivamchhapola@users.noreply.github.com>
2022-09-07 20:00:28 +0530

    Test

--- src/Eyebleach.js
+++ src/Eyebleach.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import ReactHlsPlayer from 'react-hls-player/dist';
 import ReactHtmlParser from "react-html-parser";
 import { GetPost } from './GetPost.ts';
+import Post from './Post';
 
 function Eyebleach() {
   const [ post, setPost ] = useState({});
@@ -14,20 +15,7 @@ function Eyebleach() {
   return (
     <div className="main">
       <h2>{post.title}</h2>
-      <div className="content">
-          {post.media && post.media.reddit_video && <ReactHlsPlayer
-            src={post.media.reddit_video.hls_url}
-            autoPlay={false}
-            controls={true}
-            width="100%"
-            height="100%"
-          />}
-          {post.media && post.media.oembed && ReactHtmlParser(ReactHtmlParser(post.media.oembed.html))}
-          {!post.media && /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(post.url) && <img src={post.url} alt={post.title} />}
-        <a href={"https://www.reddit.com/u/" + post.author} target="_blank" rel="noopener noreferrer">
-          <span className="author">u/{post.author}</span>
-        </a>
-      </div>
+      <Post post={this.post} />
       <div className="nav">
         <button onClick={() => {
           GetPost('eyebleach', setPost);
