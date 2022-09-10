9819f3762f5f41438c117f2e7f6219f451a58392
Shivam Chhapola <38777996+shivamchhapola@users.noreply.github.com>
2022-09-10 17:33:32 +0530

    bruh

--- src/Post.js
+++ src/Post.js
@@ -6,6 +6,7 @@ import './Post.css'
 export default function Post({post, blur}) {
   return (
     <div className="content">
+
         {post.media && post.media.reddit_video && <ReactHlsPlayer
             src={post.media.reddit_video.hls_url}
             autoPlay={false}
@@ -13,11 +14,17 @@ export default function Post({post, blur}) {
             width="100%"
             height="100%"
         />}
+
         {post.media && post.media.oembed && ReactHtmlParser(ReactHtmlParser(post.media.oembed.html))}
-      {!post.media && /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(post.url) && ((blur && <img src={post.url} alt={post.title} style={{filter: "blur(2rem)"}}/>) || <img src={post.url} alt={post.title}/>)}
-        <a href={"https://www.reddit.com/u/" + post.author} target="_blank" rel="noopener noreferrer">
-        <span className="author">u/{post.author}</span>
-        </a>
+        
+        {!post.media && /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(post.url) && 
+          ((blur && <img src={post.url} alt={post.title} style={{filter: "blur(2rem)"}}/>) || 
+          <img src={post.url} alt={post.title}/>)}
+      
+      <a href={"https://www.reddit.com/u/" + post.author} target="_blank" rel="noopener noreferrer">
+      <span className="author">u/{post.author}</span>
+      </a>
+
     </div>
   )
 }
